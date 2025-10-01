6268010bc3a922d8f39bbe140cc36c74a6e5a1bc
leilaweicman <leilaweic@hotmail.com>
2025-10-01 13:50:59 +0200

    feat(ex1): add Main with basic structure

---
+++ src/exercise1/Main.java
@@ -0,0 +1,16 @@
+package exercise1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void filterStringsContainingO() {
+
+    }
+}
