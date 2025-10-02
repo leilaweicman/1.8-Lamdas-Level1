8fde0cd4ffb50b0e300f51c52c8a681e9ec23b4e
leilaweicman <leilaweic@hotmail.com>
2025-10-02 11:38:04 +0200

    refactor(ex6-7): use SortService to delegate logic

--- src/exercise7/Main.java
+++ src/exercise7/Main.java
@@ -1,17 +1,14 @@
-package exercise6;
+package exercise7;
+
+import common.SortService;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Object> items = Arrays.asList("apple", "hi", "orange", "a", "banana", 123, 7, 42);
 
-        List<Object> sortedItems = items.stream()
-                .sorted(Comparator.comparingInt(o -> o.toString().length()))
-                .toList();
-
-        System.out.println(sortedItems);
+        System.out.println(SortService.sortByLengthDesc(items));
     }
 }
