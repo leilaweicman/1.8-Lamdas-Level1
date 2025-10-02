c2530945a391e4ac0063c32c25dede57719cad6c
leilaweicman <leilaweic@hotmail.com>
2025-10-02 10:23:49 +0200

    feat(ex1): add StringFilterService to separate logic

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("apple", "orange", "banana", "cherry", "melon");
 
-        List<String> result = words.stream().filter(word -> word.contains("o")).toList();
+        List<String> result = StringFilterService.filterStringsContainingO(words);
 
         System.out.println("Words containing 'o': " + result);
     }
---
+++ src/exercise1/StringFilterService.java
@@ -0,0 +1,12 @@
+package exercise1;
+
+import java.util.List;
+
+public class StringFilterService {
+
+    public static List<String> filterStringsContainingO(List<String> words) {
+        return words.stream()
+                .filter(word -> word.contains("o"))
+                .toList();
+    }
+}
