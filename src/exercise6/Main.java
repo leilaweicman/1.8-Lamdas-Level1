fe1faa94061c298f729cc22afd935d16e331abf9
leilaweicman <leilaweic@hotmail.com>
2025-10-02 11:20:02 +0200

    feat(ex6): add Main with items list

---
+++ src/exercise6/Main.java
@@ -0,0 +1,12 @@
+package exercise6;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Object> items = Arrays.asList("apple", "hi", "orange", "a", "banana", 123, 7, 42);
+
+        System.out.println(items);
+    }
+}
