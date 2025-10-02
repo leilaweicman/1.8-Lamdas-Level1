2bd5b0a97e9482d92966291d3621aea0b51821c3
leilaweicman <leilaweic@hotmail.com>
2025-10-02 10:25:09 +0200

    feat(ex2): add Main and StringFilterService base structure

---
+++ src/exercise2/Main.java
@@ -0,0 +1,15 @@
+package exercise2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "orange", "banana", "cherry", "melon");
+
+        List<String> result = StringFilterService.filterStringsContainingO(words);
+
+        System.out.println("Words containing 'o': " + result);
+    }
+}
