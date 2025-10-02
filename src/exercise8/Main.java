be8bef9290bf6182bb472c7eeaa105789c83a559
leilaweicman <leilaweic@hotmail.com>
2025-10-02 12:39:32 +0200

    feat(ex8): call reverse() and verify correct result

--- src/exercise8/Main.java
+++ src/exercise8/Main.java
@@ -1,2 +1,15 @@
-package exercise8;public class Main {
+package exercise8;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringReverser stringReverser = str -> new StringBuilder(str).reverse().toString();
+
+        String original = "Example";
+        String reversed = stringReverser.reverse(original);
+
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
 }
